1bf1d8da677b79fbce335be1d608ac5b9989bdc9
2004Shivampatel <shivampatel3857@gmail.com>
2025-10-30 00:33:22 +0530

    text.js

--- text.js
+++ text.js
@@ -1,2 +1,6 @@
 console.log("This is text.js file");
 console.log("This file is for testing git scm input feature in vscode");
+console.log("Make some changes to see how it works");
+console.log("Adding more lines to test the input feature");
+console.log("Final line in text.js file");  
+console.log("End of text.js file");
